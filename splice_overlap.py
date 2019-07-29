8ba3affc9c98c27e1f2e11ce05d6ca691767d37d
Ron Beavis <47994639+RonBeavis@users.noreply.github.com>
2019-07-29 19:55:50 +0000

    Add files via upload

--- splice_overlap.py
+++ splice_overlap.py
@@ -15,6 +15,7 @@ import os
 #obtain the protein sequence for the protein identified by _l
 
 def get_peptides(_l):
+	#create URL for request to GPMDB
 	url = 'https://gpmdb.thegpm.org/protein/model/%s&excel=1' % (_l)
 	session = requests.session()
 	try:
@@ -22,14 +23,16 @@ def get_peptides(_l):
 	except requests.exceptions.RequestException as e:
 		print(e)
 		return None
+	#process the TSV output from GPMDB
 	text = re.sub('\r\n','\n',r.text)
 	lines = text.split('\n')
+	#store the information in a dict with the peptide sequence as the key
 	amap = {}
 	for l in lines:
 		vs = l.split('\t')
+		#skip blank lines and the heading line
 		if len(vs) != 5 or vs[0] == 'Start':
 			continue
-#		print(vs)
 		amap[vs[4]] = vs
 	return amap
 
@@ -39,10 +42,12 @@ def get_peptides(_l):
 
 def generate_overlap(_seqs,_lbls):
 	all_seq = set()
+	#create a set of all peptide sequences in all proteins
 	for v in _seqs:
 		for a in v:
 			all_seq.add(a)
 	c = 0
+	#create top line for output
 	title = '#\tsequence\t'
 	for l in _lbls:
 		title += '%s\t' % (l)
@@ -50,31 +55,47 @@ def generate_overlap(_seqs,_lbls):
 	print(title)
 	lines = []
 	cmap = []
+	#organize information for each of the peptide sequences
 	for s in all_seq:
-		line = '%s\t' % (s)
+		#get peptide sequence
+		line = [s]
 		obs = 0
 		ts = 0
 		a = 0
+		ccs = []
+		#iterate through each protein result
 		for v in _seqs:
+			#store start positions for the peptide sequence
 			if s in v:
-				line += '%i\t' % (int(v[s][0]))
+				line.append(int(v[s][0]))
 				ts += 1
+				#get observations from the sequence
 				if int(v[s][2]) > obs:
 					obs = int(v[s][2])
+			#if peptide not in protein, record a null string
 			else:
-				line += '\t'
-			if a == 0 and s in v:
-				cmap.append(int(v[s][0]))
-			elif a == 0 and s not in v:
-				cmap.append(0)
+				line.append('')
+			#store start values for sorting
+			if s in v:
+				ccs.append(int(v[s][0]))
+			#if peptide not in protein, record start as 0 for sorting
+			elif s not in v:
+				ccs.append(0)
 			a += 1
-		line += '%i' % obs
+		#add observations information for the sequence
+		line.append(obs)
+		#store array elements for a line in output
 		lines.append(line)
+		#store start values for sorting lines
+		cmap.append(ccs)
 		c += 1
-	cs = sorted(enumerate(cmap), key=lambda x: x[1])
+	#generate indexes for sorted lines
+	cs = sorted(enumerate(cmap), key=lambda x: x[1][0])
 	c = 1
-	for c,cv in enumerate(cs):
-		print('%i\t%s' % (c+1,lines[cv[0]]))
+	#produce output
+	for c,cs1 in enumerate(cs):
+		ls = [str(a) for a in lines[cs1[0]]]
+		print('%i\t%s' % (c+1,'\t'.join(ls)))
 
 def main():
 	#deal with the command line
