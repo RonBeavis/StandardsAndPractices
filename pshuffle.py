1b41725fd5372a3e3f268cb482606315baab425b
Ron Beavis <47994639+RonBeavis@users.noreply.github.com>
2019-10-31 15:37:48 +0000

    Add files via upload

---
+++ pshuffle.py
@@ -0,0 +1,55 @@
+# a simple protein FASTA sequence shuffler
+# that preserves tryptic peptide amino acid
+# composition and location
+
+from random import shuffle
+import sys
+
+# shuffles any peptide
+def s_p(_p):
+	if len(_p) == 0:
+		return _p
+	kr = _p[len(_p)-1]
+	l = list(_p[:-1])
+	shuffle(l)
+	return ''.join(l)+kr
+
+# suffles a protein
+def scramble(_s):
+	n = ''
+	p = ''
+	for i,a in enumerate(list(_s)):
+		if a == 'K' or a == 'R':
+			n += s_p(p + a)
+			p = ''
+		else:
+			p += a
+	n += s_p(p)
+	return n
+
+if len(sys.argv) < 3 or sys.argv[1] == '-h':
+	print('usage: >python3 psuffle.py IN_FASTA OUT_FASTA')
+	exit()
+# reads a FASTA file specified on the command line
+fasta = [l.strip() for l in open(sys.argv[1],'r')]
+# opens an output FASTA file specified on the command line
+o = open(sys.argv[2],'w')
+s = ''
+l = ''
+for f in fasta:
+	if f.find('>') != -1:
+		if len(s) != 0:
+			o.write(l+' (shuffled)\n')
+			d = scramble(s)
+			o.write('\n'.join(d[i:i+50] for i in range(0, len(d), 50)))
+			o.write('\n')
+		l = f
+		s = ''
+		continue
+	s += f
+if len(s) != 0:
+	o.write(l+'\n')
+	d = scramble(s)
+	o.write('\n'.join(d[i:i+50] for i in range(0, len(d), 50)))
+	o.write('\n')
+
