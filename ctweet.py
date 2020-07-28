3e66f7cecce31ac5c20d335dca53c5e9a87a197d
Ron Beavis <47994639+RonBeavis@users.noreply.github.com>
2020-07-28 19:53:15 +0000

    Add files via upload

--- ctweet.py
+++ ctweet.py
@@ -1,8 +1,27 @@
+# ctweet.py takes tweet text from a command line.
+# Each parameter on the line are assembled into a chain of tweets.
+# For example, a command line might contain the following parameters
+'''
+>python3 ctweet.py "This is the first tweet" "This is the 2nd tweet" "/home/user/image.png"
+'''
+# The first parameter would be used to create the first tweet
+# The 2nd parameter would be added as a reply to the first tweet
+# The image file would be added to the second tweet as an attachment
+# Any parameter that contains only a file path is interpreted as an attachment
+# Any number of images can be attached to a tweet
+# Any number of tweets can be assembled into a chain
+# If the parameter is a number only, it is interpretted as a tweet
+# id number and the first line is added as a reply to that tweet.
+
+# redirect stdout to a file if you wish to maintain a log of the process
+
+# the twython package must be installed for this to work
 from twython import Twython
 import sys
 import re
 import os
 
+#auth.py contains the credentials needed to make tweets
 from auth import (
     consumer_key,
     consumer_secret,
@@ -10,6 +29,7 @@ from auth import (
     access_token_secret
 )
 
+#client_args is used for the request headers
 client_args = {
   "headers": {
     "accept-charset": "utf-8",
@@ -17,6 +37,7 @@ client_args = {
   }
 }
 
+# test to see if a parameter is an image file for attachment
 def is_image(_l):
 	if _l.find('.png') == len(_l)-4:
 		return True
@@ -26,6 +47,7 @@ def is_image(_l):
 		return True
 	return False
 
+# takes a list of lines and assembles it into tweets
 def generate_que(_ls):
 	q = []
 	v = {}
@@ -47,9 +69,14 @@ def generate_que(_ls):
 	if len(v) > 0:
 		q.append(v)
 	return q
-		
+
+# initialize a Twython object		
 twitter = Twython(consumer_key, consumer_secret, access_token, access_token_secret,client_args=client_args)
+# read parameters into an array
 ls = sys.argv[1:]
+
+# replay holds the tweet id number to the most recent tweet sent
+# 0 indicates the first tweet should be the base of the chain
 reply = 0
 try:
 	reply = int(ls[0])
@@ -58,12 +85,15 @@ try:
 except:
 	pass
 
+# assemble the tweets into a queue
 que = generate_que(ls)
 s = {}
 
+# check the reply value
 if reply > 10000:
 	s['id_str'] = str(reply)
 
+# process the queue
 for q in que:
 	rs = []
 	if 'photo' in q:
@@ -77,4 +107,5 @@ for q in que:
 	else:
 		s = twitter.update_status(status=q['message'],media_ids=rs)
 	print('id = %s' % (s['id_str']))
+
 
