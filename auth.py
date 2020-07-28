12d86b0933e62f33dee048072a51de56d931a566
Ron Beavis <47994639+RonBeavis@users.noreply.github.com>
2020-07-28 19:43:59 +0000

    Add files via upload

---
+++ auth.py
@@ -0,0 +1,6 @@
+consumer_key = 'key_from_twitter'
+consumer_secret = 'secret_from_twitter'
+TWITTER_BEARER_TOKEN = 'token_from_twitter'
+access_token = 'access_token_from_twitter'
+access_token_secret = 'acess_token_secret_from_twitter'
+
