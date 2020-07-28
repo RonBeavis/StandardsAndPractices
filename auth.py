5a02aa55c4f51b8bde6c8eb1dc27ec5bd549375f
Ron Beavis <47994639+RonBeavis@users.noreply.github.com>
2020-07-28 19:45:37 +0000

    Add files via upload

--- auth.py
+++ auth.py
@@ -1,3 +1,7 @@
+# these are strings supplied by Twitter that allow access to the platform
+# this file is required by ftweet.py and ctweet.py
+# the values here are place holders: they will no provide twitter access
+
 consumer_key = 'key_from_twitter'
 consumer_secret = 'secret_from_twitter'
 TWITTER_BEARER_TOKEN = 'token_from_twitter'
