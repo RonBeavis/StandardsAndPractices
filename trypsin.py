0a3dafce9befdccc46055ae2d0b9bc6a14b4e838
Ron Beavis <47994639+RonBeavis@users.noreply.github.com>
2021-10-12 20:42:33 +0000

    Add files via upload

--- trypsin.py
+++ trypsin.py
@@ -15,28 +15,34 @@ def trypsin(_seq):
 	lseq = len(_seq)
 	seqs = list(_seq)
 	peps = []
+	# iterate through the sequence
 	for i,res in enumerate(seqs):
 		if i == lseq - 1:
 			continue
+		# if you just passed a cleavage site, act
 		if i == 0 or (seqs[i-1] in ss and seqs[i] not in cbad):
 			j = i + 1
+			# is the next residue a cleavage site too
 			if j < lseq-1 and seqs[j] in ss and seqs[j+1] not in cbad:
 				peps.append({'seq':'%s' % (_seq[i:j+1]),'f':i+1,'l':j+1})
 				if i == 0:
 					j += 1
 				else:
 					continue
+			# find the next cleavage site
 			while j < lseq-1 and not (seqs[j] in ss and seqs[j+1] not in cbad):
 				j += 1
 			if j < lseq -2:
 				peps.append({'seq':'%s' % (_seq[i:j+1]),'f':i+1,'l':j+1})
 			j += 1
+			# deal with the last residue cleavage problem
 			if j < lseq-1 and seqs[j] in ss and seqs[i+1] not in cbad:
 				peps.append({'seq':'%s' % (_seq[i:j+1]),'f':i+1,'l':j+1})
 			elif j == lseq - 1:
 				peps.append({'seq':'%s' % (_seq[i:j+1]),'f':i+1,'l':j+1})
 		else:
 			pass
+	# make sure everything is in order
 	peps = [p for p in sorted(peps, key=lambda k: k['f'])]
 	return peps
 
